f952249d4d3ad7373be7c254ad6309e43a5e93d6
Ahmet Cagan <ahmet.cagan@yahoo.com>
2023-05-01 15:13:34 +0200

    JSON format and HTTP error code parts are done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const v1WorkoutRouter = require('./v1/routes/workoutRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// For testing purposes
+app.use(bodyParser.json());
+app.use('/api/v1/workouts', v1WorkoutRouter);
 
-app.get('/', (req, res) => {
-  res.send("<h2>It's working! </h2>");
-});
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
 });
